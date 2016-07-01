53c29d75e234d1e23aec77e84185da7ad3e7e0ed
TiagoXistech <tiago@xistech.com.br>
2016-07-01 08:46:13 -0300

    alteração

---
+++ Camada.Dominio/Entidades/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Camada.Dominio.Entidades
+{
+    class Produto
+    {
+        public int ProdutoId { get; set; } 
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
